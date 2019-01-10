7e30609374f58029497987255157991cf12b416f
Navid Haghdadi <n.haghdadi@gmail.com>
2019-01-10 13:14:46 +1100

    First commit
    Bill calculator function and an example file

---
+++ Bill_Calc.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import time
+
+
+def bill_calculator(load_profile, tariff):
+
+    def pre_processing_load(load_profile):
+
+        # placeholder for a quick quality check function for load profile
+        # make sure it is kwh
+        # make sure it is one year
+        # make sure it doesn't have missing value or changing the missing values to zero or to average
+        # make sure the name is Load
+        # time interval is half hour
+        return load_profile
+
+    def fr_calc(load_profile, tariff):
+
+        Results = pd.DataFrame(load_profile[load_profile > 0].sum(), columns=['Annual_kWh'])
+        Results['Annual_kWh_exp'] = -1 * load_profile[load_profile < 0].sum()
+        Results['DailyCharge'] = len(load_profile.index.normalize().unique()) * tariff['Parameters']['Daily']['Value']
+        Results['EnergyCharge'] = Results['Annual_kWh'] * tariff['Parameters']['Energy']['Value']
+        Results['EnergyCharge_Discounted'] = Results['EnergyCharge'] * (1 - tariff['Discount (%)'] / 100)
+        Results['Fit_Rebate'] = Results['Annual_kWh_exp'] * tariff['Parameters']['FiT']['Value']
+        Results['Bill'] = Results['DailyCharge'] + Results['EnergyCharge'] + Results['Fit_Rebate']
+
+        return Results
+
+    def tou_calc(load_profile, tariff):
+        t0 = time.time()
+        Results = pd.DataFrame(load_profile[load_profile > 0].sum(), columns=['Annual_kWh'])
+        Results['Annual_kWh_exp'] = -1 * load_profile[load_profile < 0].sum()
+        Results['DailyCharge'] = len(load_profile.index.normalize().unique()) * tariff['Parameters']['Daily']['Value']
+
+        load_profile['time_ind'] = 0
+
+        all_tou_charge = 0
+        load_profile_TI = pd.DataFrame()
+        load_profile_TI_Charge = pd.DataFrame()
+        ti = 0
+        for k, v in tariff['Parameters']['Energy'].items():
+            this_part = tariff['Parameters']['Energy'][k].copy()
+            ti += 1
+            for k2, v2, in this_part['TimeIntervals'].items():
+                start_hour = int(this_part['TimeIntervals'][k2][0][0:2])
+                if start_hour == 24:
+                    start_hour = 0
+                start_min = int(this_part['TimeIntervals'][k2][0][3:5])
+                end_hour = int(this_part['TimeIntervals'][k2][1][0:2])
+                if end_hour == 0:
+                    end_hour = 24
+                end_min = int(this_part['TimeIntervals'][k2][1][3:5])
+                if this_part['Weekday']:
+                    if start_hour <= end_hour:
+                        load_profile['time_ind'] = np.where((load_profile.index.weekday < 5) &
+                                                            (load_profile.index.month.isin(this_part['Month'])) &
+                                                            (((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              >= (60 * start_hour + start_min)) &
+                                                             ((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              < (60 * end_hour + end_min))), ti,
+                                                            load_profile['time_ind'])
+                    else:
+                        load_profile['time_ind'] = np.where((load_profile.index.weekday < 5) &
+                                                            (load_profile.index.month.isin(this_part['Month'])) &
+                                                            (((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              >= (60 * start_hour + start_min)) |
+                                                             ((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              < (60 * end_hour + end_min))), ti,
+                                                            load_profile['time_ind'])
+                if this_part['Weekend']:
+                    if start_hour <= end_hour:
+                        load_profile['time_ind'] = np.where((load_profile.index.weekday >= 5) &
+                                                            (load_profile.index.month.isin(this_part['Month'])) &
+                                                            (((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              >= (60 * start_hour + start_min)) &
+                                                             ((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              < (60 * end_hour + end_min))), ti,
+                                                            load_profile['time_ind'])
+                    else:
+                        load_profile['time_ind'] = np.where((load_profile.index.weekday >= 5) &
+                                                            (load_profile.index.month.isin(this_part['Month'])) &
+                                                            (((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              >= (60 * start_hour + start_min)) |
+                                                             ((60 * load_profile.index.hour + load_profile.index.minute)
+                                                              < (60 * end_hour + end_min))), ti,
+                                                            load_profile['time_ind'])
+
+            load_profile_TI[k] = load_profile.loc[load_profile['time_ind'] == ti, :].sum()
+            load_profile_TI_Charge[k] = this_part['Value'] * load_profile_TI[k]
+
+        Results['EnergyCharge'] = load_profile_TI_Charge.sum(axis=1)
+        Results['EnergyCharge_Discounted'] = Results['EnergyCharge'] * (1 - tariff['Discount (%)'] / 100)
+
+        Results['Fit_Rebate'] = Results['Annual_kWh_exp'] * tariff['Parameters']['FiT']['Value']
+        Results['Bill'] = Results['DailyCharge'] + Results['EnergyCharge'] + Results['Fit_Rebate']
+        print(time.time() - t0)
+
+        return Results
+
+    # Checking the type and run the appropriate function
+    if tariff['Type'] == 'Flat_rate':
+        Results = fr_calc(load_profile, tariff)
+    elif tariff['Type'] == 'TOU':
+
+        Results = tou_calc(load_profile, tariff)
+    else:
+        Results = 'Error'
+
+    return Results
---
+++ Example.py
@@ -0,0 +1,64 @@
+
+import pandas as pd
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import requests
+from Bill_Calc import bill_calculator as calc
+
+
+t0 = time.time()
+TestData = pd.read_csv('D:/Dropbox/Database/Load/SGSC/ProcessedData/General_filtered.csv')
+print(time.time()-t0) # takes 59 s to load
+
+
+SGSC_kWh = pd.concat([pd.to_datetime(TestData['READING_DATETIME']), TestData.iloc[:, 1:-1].astype('float32')],
+                     axis=1).reset_index(drop=True)
+SGSC_kWh_sum = pd.concat([SGSC_kWh['READING_DATETIME'], pd.DataFrame(np.nansum(SGSC_kWh.iloc[:, 1:].values, axis=1),
+                                                                     columns=['Sum'])], axis=1).reset_index(drop=True)
+
+#plt.figure()
+#plt.plot(SGSC_kWh['READING_DATETIME'], SGSC_kWh_sum['Sum'])
+
+
+SGSC_kWh_Daily = np.nansum(SGSC_kWh.iloc[:, 1:].values, axis=0)
+
+#plt.figure()
+#plt.hist(SGSC_kWh_Daily, 100)
+
+# filter to only 2013
+SGSC_kWh_2013 = SGSC_kWh[SGSC_kWh['READING_DATETIME'] > '2013-01-01 00:00:00'].copy().set_index('READING_DATETIME')
+# finding the missing values
+NumNonNan = (len(SGSC_kWh_2013) - SGSC_kWh_2013.count())/len(SGSC_kWh_2013.iloc[:, 1:-1])
+# filtering those which have more than 5% missing value
+GoodHomes = NumNonNan.index[NumNonNan < 0.05].tolist()
+
+# sample
+#plt.figure()
+#plt.plot(SGSC_kWh_2013.index, SGSC_kWh_2013['8144683'])
+
+# filter to good homes
+SGSC_kWh_2013 = SGSC_kWh_2013.iloc[:, SGSC_kWh_2013.columns.isin(GoodHomes)]
+
+# testing a tariff
+
+# Tariff - this will be selected by used from a list of tariffs in the dropdown list here we use a sample for testing
+
+all_tariffs = requests.get('http://api.ceem.org.au/elec-tariffs/retail')
+
+# sample flat rate tariff
+Tariff_name = "Origin Flat Rate NSW (Endeavour area)"
+# sample tou tariff
+Tariff_name = "Energy Locals TOU ACT"
+
+all_tariffs_list = all_tariffs.json()
+for i in range(len(all_tariffs_list)):
+    if all_tariffs_list[i]['Name'] == Tariff_name:
+        selected_tariff = all_tariffs_list[i]
+
+
+tariff = selected_tariff
+load_profile = SGSC_kWh_2013.copy()
+load_profile.info()
+
+Result = calc(load_profile, tariff)
